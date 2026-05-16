8dca43d64c58c89f678b9b6517f9b3c25981e95e
TaliaMendes <taliafmendes09@gmail.com>
2026-05-16 18:56:18 -0300

    adiciona testes de unidade

--- SortedSubsequence3.test.js
+++ SortedSubsequence3.test.js
@@ -7,3 +7,12 @@ test("encontra uma subsequencia crescente de tamanho 3", () => {
 test("retorna um array vazio quando não há subsequencia crescente de tamanho 3", () => {
   expect(find3Numbers([5, 4, 3, 2, 1])).toEqual([]);
 });
+
+test("retorna um array vazio, quando os valores estiverem em ordem estritamente decrescente", () => {
+  expect(find3Numbers([10, 9, 8, 7, 6])).toEqual([]);
+});
+
+test("retorna um array vazio, quando os elementos forem todos iguais", () => {
+  expect(find3Numbers([4, 4, 4, 4])).toEqual([]);
+});
+
